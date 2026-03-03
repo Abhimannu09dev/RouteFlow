986d989b6f47e3dd219c737f5125bfcbb56bd336
Abhimannu Singh Kunwar <anmolkunwar07@gmail.com>
2026-03-04 00:38:38 +0545

    fix: resolved connection issue

--- backend/websocket/index.js
+++ backend/websocket/index.js
@@ -1,4 +1,14 @@
+require("dotenv").config({ path: "../.env" });
 const WebSocket = require("ws");
+const jwt = require("jsonwebtoken");
+const {
+  addUser,
+  removeUser,
+  getUserSocket,
+  getUsersByRole,
+} = require("./connectionManager");
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 let wss;
 
@@ -7,31 +17,36 @@ function initWebSocket(server) {
 
   wss.on("connection", (ws, req) => {
     console.log("Client connected");
-    try{
-        const url = new URL(req.url, `http://${req.headers.host}`);
+
+    try {
+      const url = new URL(req.url, `http://${req.headers.host}`);
       const token = url.searchParams.get("token");
-        if (!token) {
-            ws.close(1008, "Token missing");
-            return;
-        }
-        // Here you would verify the token and extract user info
-        const decoded = jwt.verify(token, JWT_SECRET);
-        const userId = decoded.id;
-        const role = decoded.role;
-        addUser(userId, ws, role);
-        console.log(`User ${userId} connected with role ${role}`);
-    } catch (error) {
-        ws.close(1008, "Invalid token");
+      if (!token) {
+        ws.close(1008, "Token missing");
         return;
+      }
+      const decoded = jwt.verify(token, JWT_SECRET);
+
+      const userId = decoded.id;
+      const role = decoded.role;
+
+      addUser(userId, ws, role);
+
+      console.log(`User ${userId} connected with role ${role}`);
+    } catch (error) {
+      console.log("JWT ERROR:", error.message);
+      ws.close(1008, error.message);
+      return;
     }
 
     ws.on("close", () => {
       console.log("Client disconnected");
+      removeUser(ws);
     });
   });
 }
 
-function broadcast(data) {s
+function broadcast(data) {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(data));
